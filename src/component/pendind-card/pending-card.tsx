6ee2db492106db0823f125c04a60998642c6eb91
JK <74600256+Programmer-JK@users.noreply.github.com>
2025-02-05 22:52:08 +0900

    [feat] column 상태값 ui 추가

--- src/component/pendind-card/pending-card.tsx
+++ src/component/pendind-card/pending-card.tsx
@@ -1,5 +1,5 @@
 import { CardProps, TagTypes } from "@/type/common";
-import { getTagColorClass } from "@/util/common";
+import { getStateColorClass, getTagColorClass } from "@/util/common";
 import { X } from "lucide-react";
 import { useState } from "react";
 import Modal from "../modal/modal";
@@ -21,14 +21,23 @@ const PendingCard = ({ column }: CardProps) => {
     <div>
       <div
         className="
-    flex flex-col items-end 
+    flex flex-col items-end gap-2
     w-full my-1 p-2 
     bg-white rounded-md 
     shadow-md shadow-black/40
     "
         onClick={openCreateColumnHandler}
       >
-        <X size={16} onClick={handleDeleteColumn} />
+        <div className="flex flex-row justify-between w-full">
+          <div
+            className={`px-2 py-0.5 rounded-md border text-sm ${getStateColorClass(
+              column.state
+            )}`}
+          >
+            {column.state}
+          </div>
+          <X size={16} onClick={handleDeleteColumn} />
+        </div>
         <div className="w-full flex flex-col items-start">
           <div className="flex flex-wrap whitespace-nowrap">
             {column.tags &&
